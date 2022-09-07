9534a6e8d66c27ab934da286e35cb132eac56efb
Shinji Okazaki <oshinji871@gmail.com>
2022-09-07 10:55:23 +0900

    Created ActionMailer::Preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,14 @@
 class UserMailer < ApplicationMailer
     default from: 'notifications@example.com'
 
-    def welcome_email
-        @user = params[:user]
-        @user = 'http://example.com/login'
+    # def welcome_email
+    #     @user = params[:user]
+    #     @user = 'http://example.com/login'
+    #     mail(to: @user.email, subject: '私の素敵なサイトへようこそ')
+    # end
+    
+    def welcome_email(user)
+        @user = user
         mail(to: @user.email, subject: '私の素敵なサイトへようこそ')
     end
-    
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+    def user
+        user = User.new(name: '侍 太郎', email: 'tarou@gmail.com', login: '000000')
 
+        UserMailer.welcome_email(user)
+    end
+    
 end
